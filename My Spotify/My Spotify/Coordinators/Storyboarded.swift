bc298aa84a06555794f90c58525111a7b826d65f
Krunal Patel <kbpatel5802@gmail.com>
2023-07-04 14:37:04 +0530

    TE7-T1151: Add initial project setup

---
+++ My Spotify/My Spotify/Coordinators/Storyboarded.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  My Spotify
+//
+//  Created by Krunal Patel on 04/07/23.
+//
+
+import UIKit
+
+enum Storyboard: String {
+    case home = "Home"
+}
+
+protocol Storyboarded {
+    static func instantiate(from: Storyboard) -> Self
+}
+
+extension Storyboarded where Self: UIViewController {
+    
+    static func instantiate(from storyboard: Storyboard) -> Self {
+        // this pulls out "MyApp.MyViewController"
+        let fullName = NSStringFromClass(self)
+
+        // this splits by the dot and uses everything after, giving "MyViewController"
+        let className = fullName.components(separatedBy: ".")[1]
+
+        // load our storyboard
+        let storyboard = UIStoryboard(name: storyboard.rawValue, bundle: Bundle.main)
+
+        // instantiate a view controller with that identifier, and force cast as the type that was requested
+        return storyboard.instantiateViewController(withIdentifier: className) as! Self
+    }
+}
---
+++ My Spotify/My Spotify/Modules/Home/Controllers/HomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  My Spotify
+//
+//  Created by Krunal Patel on 04/07/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController, Storyboarded {
+    
+    // MARK: - Vars & Lets
+    var homeCoordinator: HomeCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
